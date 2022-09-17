d44f4a175338ba02a8a0906544db95d0ceec6ea8
Peter Neville <neville.peter.2000@gmail.com>
2022-09-17 01:01:46 -0500

    cambios finales del portfolio

--- src/components/Knowledge.js
+++ src/components/Knowledge.js
@@ -3,13 +3,15 @@ import Html from './Images/html.png';
 import Java from './Images/javas.png';
 import Sass from './Images/sass.png';
 import Css from './Images/css.png';
+import Jest from './Images/jest.png';
+import ReactTesting from './Images/reactTesting.png';
 import './Knowledge.css';
 
 
 function knowledge (){
 
   return (
-    <div className='knowledge'>
+    <div className='knowledge' id="know">
         <h2>My knowledge</h2>
         <div className='icons'>
          <img src={Html} alt='html' />
@@ -18,6 +20,10 @@ function knowledge (){
          <img src={React1} alt='react' />
          <img src={Sass} alt='sass' />
         </div> 
+        <div>
+        <img src={Jest} alt='jest' />
+        <img src={ReactTesting} alt='reactTesting' />
+        </div>
       </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,9 @@ function NavBar (){
           </div>
           <div className="menu_bar">
             <a href="#portfolio">Portfolio</a>
+            <a href="#know">My Knowledge</a>
             <a href='#work'>let's Work</a>
+            <a href="mailto:neville.peter.2000@gmail.com">Contact Me</a>
           </div>
         </nav>
         
